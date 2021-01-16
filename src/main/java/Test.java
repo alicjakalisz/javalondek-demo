5f233f461f04bfe1a1b61f8dab42f2907e10a959
dvirgiln <david.virgil.naranjo@gmail.com>
2021-01-16 10:29:18 +0000

    [init] - initial project commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+// never ever use default package!!!! You cannot import such class!!!
+    public static void main(String[] args) {
+        System.out.println("Wrong hello world!!!");
+    }
+}
---
+++ src/main/java/pl/sda/javalondek4/java_demo/HelloYou.java
@@ -0,0 +1,8 @@
+//root package = groupID + artifacktId
+package pl.sda.javalondek4.java_demo;
+
+public class HelloYou {
+    public static void main(String[] args) {
+        System.out.println("Witajcie na zajeciach u manka:)");
+    }
+}
