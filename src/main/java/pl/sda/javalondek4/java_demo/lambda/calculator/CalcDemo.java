4edd87e12d4b278e76a95fcaa63763d6dc13d7c6
Alicja Kalisz <alicja.kalisz@hotmail.co.uk>
2021-01-23 08:28:33 +0000

    [add] - homework playable

--- src/main/java/pl/sda/javalondek4/java_demo/lambda/calculator/CalcDemo.java
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/calculator/CalcDemo.java
@@ -34,5 +34,13 @@ public class CalcDemo {
 
         Operation<Double> divisionOperation = (first, second) -> first/second;
 
+        Playable<Long> footballPlayer= (speed)-> System.out.println("Football players speed is: " + speed + "km/h");
+        Playable<String> gamesPlayer= (name)-> System.out.println("Game Player "+ name + " is playing");
+        Playable<Integer> loser=(numerOfLostGames)-> System.out.println("Loser is losing his " + numerOfLostGames+ "th game.");
+
+        footballPlayer.play(10L);
+        gamesPlayer.play("Bartek");
+        loser.play(5);
+
     }
 }
